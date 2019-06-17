30c1737f96920ab987111bc3f3aa3d66445c2233
gaokang <970720947@qq.com>
2019-06-17 10:22:23 +0800

    demo增加对网络返回数据的toast提示

--- app/src/main/java/com/imgod1/commonlib/MainActivity.kt
+++ app/src/main/java/com/imgod1/commonlib/MainActivity.kt
@@ -2,11 +2,13 @@ package com.imgod1.commonlib
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.imgod1.commlib.base.BaseResponse
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MainContract.View {
     override fun onGetDataSuccess(resq: BaseResponse<Any>) {
+        Toast.makeText(this, resq.msg, Toast.LENGTH_LONG).show()
     }
 
     override fun showLoading() {
